b66642d9f8d87b8cd4a7d56c4ae90949fd35037e
Christopher <chris.brower.96@gmail.com>
2022-12-15 10:46:25 -0700

    commit tree 2fd4ae6f9231207a859bc1b4f112b78d2391b9cc

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name);
+
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name);
+}
+
+function appendCat(name){
+    const catsPlus = [...cats.slice(), name];
+    return catsPlus
+}
+
+function prependCat(name){
+    const catsPlus = [name, ...cats.slice()];
+    return catsPlus
+}
+
+function removeLastCat(){
+    const catsMinus = [0, cats.slice(cats.length-1)];
+    return catsMinus;
+}
