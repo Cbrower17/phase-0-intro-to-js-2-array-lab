684bc705403a66d146231aa7340a1a86c769163a
Christopher <chris.brower.96@gmail.com>
2023-02-01 10:20:40 -0700

    redo

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function  destructivelyAppendCat(name){
+    cats.push(name);
+    console.log(cats);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    console.log(cats);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const catCopy = [...cats, name];
+    return catCopy;
+    
+}
+
+function prependCat(name){
+    const copyCat = [name, ...cats]
+    return copyCat;
+}
+
+function removeLastCat(){
+    const copyCat = cats.slice(0,cats.length-1);
+    console.log(copyCat);
+    return copyCat;
+}
+
+// removeLastCat()
+
+
+function removeFirstCat(){
+    const copyCat = cats.slice(1)
+    console.log(copyCat);
+    return copyCat; 
+}
